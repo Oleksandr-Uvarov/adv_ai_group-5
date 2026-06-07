019bf4bae8d672fe6223fd9ddbae271edf68386a
Oleksandr <sashka15002@gmail.com>
2026-06-07 15:36:41 +0200

    remoe old files.

--- coords/train_coords.py
+++
@@ -1,8 +0,0 @@
-from coords.roguelikeenvcoords import RoguelikeEnvCoords
-from stable_baselines3 import PPO
-
-env = RoguelikeEnvCoords(grid_size=10)
-model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=300_000)
-model.save("roguelike_coords")
-
